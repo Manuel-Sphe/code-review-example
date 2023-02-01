1f8db79c695b07593d075c0ce0b123027b570e51
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-02-01 11:28:45 +0200

    Section A Option 1

---
+++ Section_A/anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        result = {}
+        for i in strs:
+            x = "".join(sorted())
+            if x in result:
+                result[x].append(i)
+            else :
+                result[x] = [i]
+        return list(result.values())
+ob1 = Solution()
+print(ob1.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
